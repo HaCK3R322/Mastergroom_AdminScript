44658f06e3e9bd681489316c4fce924ec6a5f524
dictator_zx <ivanandrosovv@gmail.com>
2022-08-04 19:30:29 +0300

    Help frame
    - Now separates text by separator and opens new frames for each fragment

--- src/main/java/com/androsov/gui/frames/help/HelpFrame.java
+++ src/main/java/com/androsov/gui/frames/help/HelpFrame.java
@@ -3,6 +3,7 @@ package com.androsov.gui.frames.help;
 import com.androsov.gui.ViewConfig;
 import com.androsov.gui.frames.DefaultFrame;
 import com.androsov.gui.frames.EscClosableDefaultFrame;
+import info.clearthought.layout.TableLayout;
 
 import javax.swing.*;
 import java.awt.*;
@@ -16,7 +17,16 @@ public class HelpFrame extends EscClosableDefaultFrame {
         this.setTitle(helpTopic);
 
         JTextPane textPanel = new JTextPane();
+
+        if(helpText.contains(HelpManager.getSeparator())) {
+            String text2 = helpText.substring(helpText.indexOf(HelpManager.getSeparator()) + HelpManager.getSeparator().length());
+            HelpFrame newHelpFrame = new HelpFrame(helpTopic, text2);
+            newHelpFrame.setBounds(this.getX() + 50, this.getY() + 50, newHelpFrame.getWidth(), newHelpFrame.getHeight());
+
+            helpText = helpText.substring(0, helpText.indexOf(HelpManager.getSeparator()));
+        }
         textPanel.setText(helpText);
+
         textPanel.setFont(new Font("Monospaced", Font.PLAIN, 20));
         textPanel.setEditable(false);
         textPanel.setOpaque(true);
@@ -29,6 +39,8 @@ public class HelpFrame extends EscClosableDefaultFrame {
         scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         scrollPane.setOpaque(true);
         scrollPane.setBackground(viewConfig.getBackgroundColor());
+
+
 
         this.add(scrollPane);
         this.setVisible(true);
