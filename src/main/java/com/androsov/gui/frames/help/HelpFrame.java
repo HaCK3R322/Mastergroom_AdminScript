c7f41f3ce9781d26c9365b837dcb42ff1103b9ac
dictator_zx <ivanandrosovv@gmail.com>
2022-08-04 19:23:10 +0300

    Help frame
    - text font bigger, now scrollable

--- src/main/java/com/androsov/gui/frames/help/HelpFrame.java
+++ src/main/java/com/androsov/gui/frames/help/HelpFrame.java
@@ -5,6 +5,7 @@ import com.androsov.gui.frames.DefaultFrame;
 import com.androsov.gui.frames.EscClosableDefaultFrame;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class HelpFrame extends EscClosableDefaultFrame {
     private static final ViewConfig viewConfig = ViewConfig.getInstance();
@@ -16,12 +17,20 @@ public class HelpFrame extends EscClosableDefaultFrame {
 
         JTextPane textPanel = new JTextPane();
         textPanel.setText(helpText);
+        textPanel.setFont(new Font("Monospaced", Font.PLAIN, 20));
         textPanel.setEditable(false);
         textPanel.setOpaque(true);
         textPanel.setBackground(viewConfig.getBackgroundColor());
+        textPanel.setCaretPosition(0);
 
-        this.add(textPanel);
+        JScrollPane scrollPane = new JScrollPane(textPanel);
+        scrollPane.setPreferredSize(new Dimension(viewConfig.getHelpFrameSizeX(), viewConfig.getHelpFrameSizeY()));
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setOpaque(true);
+        scrollPane.setBackground(viewConfig.getBackgroundColor());
 
+        this.add(scrollPane);
         this.setVisible(true);
     }
 }
